912a4282243340b99011aeef6e241c37ff788f56
Gustavo <gugasparotto@hotmail.com>
2023-03-04 18:44:13 -0300

    criado a view de registro de movimentações dos produtos e suas logicas

--- app/Http/Controllers/ProductMovementController.php
+++ app/Http/Controllers/ProductMovementController.php
@@ -6,11 +6,5 @@ use Illuminate\Http\Request;
 
 class ProductMovementController extends Controller
 {
-    public function store(Request $request)
-    {
-        $product= Product::findOrFail($request->user_id);
-        $product->addQuantidade($request->quantity);
-        $product->save();
-        return redirect('/products');
-    }
+    
 }
